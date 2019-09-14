e37bc378e4f8f36ae1656b90b213df5516adbeab
Andy0514 <wanghaolinsh@icloud.com>
2019-09-14 16:38:25 -0400

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
     grade: Number, //11: grade 11, 12: grade 12, 1: uni 1st year, 2: uni 2nd year, 3: uni 3rd year, 4: uni 4th year, 0: not in school
     softwareSkill: Number,
     hardwareSkill: Number,
+    winningInterest: Boolean,
     personalDescription: String,
     
     events: [
