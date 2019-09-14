c274dd8c5c2e766bc5f9b15f5e8a5e2230c97195
Paco <paco.sanchez.ram@gmail.com>
2019-09-14 16:28:12 -0500

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -34,35 +34,7 @@ router.post("/register", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-    res.redirect("login");
+    res.render("landing");
 });
 
-function pullData(){
-
-var SurveyMonkeyAPI = require('surveymonkey').SurveyMonkeyAPI;
-
-var accessToken = 'EOHBd59CnkOdieYFC8v0UJiK8U7NTkGmNT8y2ImeSnTxsm4Wyq--mTlr7..TFXU1Hr3k-dpPfLAbzKb3qZg.KZc-gudT0ovEVdpPdYJR.TUK7jlGY4QS1n0k73wQBCLT';
-
-try { 
-    var api = new SurveyMonkeyAPI(accessToken, { version : 'v3', secure : false });
-} catch (error) {
-    console.log(error.message);
-}
-
-
-// or with a survey id
-api.getResponses({id:'188715488'},function (error, data) {
-    if (error)
-        console.log(error);
-    else
-        var entireText = JSON.stringify(data); // Do something with your data!
-        var fs = require('fs');
-    fs.writeFile("test.json", entireText, function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
-});
-    
-}
 module.exports = router;
